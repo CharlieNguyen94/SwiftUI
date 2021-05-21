b3cdb20f8da655c2e8b0d6d7c76981047644e780
CharlieNguyen94 <35964099+CharlieNguyen94@users.noreply.github.com>
2021-05-21 16:19:22 +0100

    Add files via upload

---
+++ SwiftUI-FormApp/SwiftUI-FormApp/SwiftUI_FormAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FormAppApp.swift
+//  SwiftUI-FormApp
+//
+//  Created by Charlie Nguyen on 21/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FormAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
